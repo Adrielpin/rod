df3168a9c858467a22eb14c919973298c2718ada
roddhp <roddhp@gmail.com>
2017-02-24 15:47:33 -0300

    alteração/correção texto e imagem
    Alterado imagem e ordem de exibição das imagens e correção de texto

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -9,24 +9,24 @@
     <li>
       <img src="https://drscdn.500px.org/photo/137899643/q%3D80_m%3D2000/5aa38e529c2b02af70c7a5c578bf7c1b">
       <div class="caption center-align">
-        <h2>Inspiração e arte!</h2>
-        <h4 class="light grey-text text-lighten-3">Fotografia e família.</h4>
-      </div>
-    </li>
-
-    <li>
-      <img src="https://drscdn.500px.org/photo/141475619/q%3D80_m%3D1500/5c850baff4a8b91f65fb0a4d9192a6ea">
-      <div class="caption left-align">
-        <h2>Aniversario infantil</h2>
-        <h4 class="light grey-text text-lighten-3">Captura de emoção.</h4>
+        <h2>Inspiração e Arte</h2>
+        <h4 class="light grey-text text-lighten-3">Fotografia e Família</h4>
       </div>
     </li>
 
     <li>
       <img src="https://drscdn.500px.org/photo/163469437/q%3D80_m%3D2000/ae526b6695a338c99f14970066e703c2">
       <div class="caption right-align">
-        <h2>Criatividade</h2>
-        <h4 class="light grey-text text-lighten-3">Fotos exclusivas.</h4>
+        <h2>Natureza</h2>
+        <h4 class="light grey-text text-lighten-3">Criatividade</h4>
+      </div>
+    </li>
+
+    <li>
+      <img src="https://drscdn.500px.org/photo/141475619/q%3D80_m%3D1500/5c850baff4a8b91f65fb0a4d9192a6ea">
+      <div class="caption left-align">
+        <h2>Festa Infantil</h2>
+        <h4 class="light grey-text text-lighten-3">Captura de emoção.</h4>
       </div>
     </li>
 
