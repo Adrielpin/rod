4f4fd742915bad6c43fde88ef99d714e86470093
Adriel Pinheiro <adriel@belladasemana.com.br>
2017-02-24 15:30:06 -0300

    menu alterado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,22 +5,23 @@
   <nav>
     <div class="nav-wrapper container">
 
+	<a href="{{route('home')}}" class="brand-logo" style="padding: 10px"><img src="{{ Storage::url('img/logo.png') }}"></a>
+      	<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      <ul class="left hide-on-med-and-down">
-        <li><a href="{{route('home')}}" class="brand-logo" style="padding: 10px"><img src="{{ Storage::url('img/logo.png') }}"></a></li>
-      </ul>
       <ul class="right hide-on-med-and-down">
         <li><a href="{{route('home')}}">Quem somos</a></li>
         <li><a href="{{route('galeria')}}">Galeria</a></li>
         <li><a href="{{route('home')}}">Contato</a></li>
       </ul>
+
       <ul class="side-nav" id="mobile-demo">
         <li><a href="{{route('home')}}">Quem somos</a></li>
         <li><a href="{{route('galeria')}}">Galeria</a></li>
         <li><a href="{{route('home')}}">Contato</a></li>
       </ul>
+
     </div>
+
   </nav>
 </header>
 <section>
